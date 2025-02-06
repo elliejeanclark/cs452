b6fff35674b48b096e6f263ef69d6f1e4b8fe491
Ellie <elinorj@byu.edu>
2025-02-05 17:47:10 -0700

    changed where I did the project

--- recommender/db_query.py
+++
@@ -1,9 +0,0 @@
-## This script is used to query the database
-import os
-import psycopg2
-from dotenv import load_dotenv
-
-load_dotenv()
-
-CONNECTION = None # paste connection string here or read from .env file
-
